7001f576423d5db201a891f63e90966ad12ddde1
schaphil <philipp@vier1.de>
2022-02-09 12:04:07 +0100

    Install script

---
+++ zsh_install.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# zsh installer script
+sudo apt-get install wget zsh git && zsh --version
+sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+chsh -s $(which zsh)
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+wget https://raw.githubusercontent.com/schaphil/zsh-installer/master/zshrc.txt -O ~/.zshrc
