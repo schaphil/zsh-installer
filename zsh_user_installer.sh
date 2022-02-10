fd5e34f2ced49f53295b1de660aa8dec32badc72
schaphil <philipp@vier1.de>
2022-02-10 11:15:40 +0100

    Delete zsh_user_installer.sh

--- zsh_user_installer.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-# zsh installer script
-sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-chsh -s $(which zsh)
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-wget https://raw.githubusercontent.com/schaphil/zsh-installer/master/zshrc.txt -O ~/.zshrc
